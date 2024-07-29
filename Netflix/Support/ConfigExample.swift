949510e9702de94fbac7877c3ca152d91dd2439b
ALİ AHMET ERDOĞDU <082aliahmet4@gmail.com>
2024-07-29 20:47:03 +0300

    MVVM training

---
+++ Netflix/Support/ConfigExample.swift
@@ -0,0 +1,14 @@
+//
+//  ConfigExample.swift
+//  Netflix
+//
+//  Created by Ali ahmet Erdoğdu on 29.07.2024.
+//
+
+import Foundation
+
+
+// Please change the name of class as "Config" then put your api key
+class ConfigExample {
+   static let apiKey: String = "apiKey"
+}
